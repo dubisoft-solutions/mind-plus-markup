ee1b4f44bf9b2041d54ab4ad74631800280a0483
Ivan Dubynets <ivan.dubynets@gmail.com>
2022-12-12 10:52:49 +0100

    delete slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 //= ../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
 //= ../../node_modules/jquery/dist/jquery.slim.min.js
-//= ../../node_modules/tiny-slider/dist/min/tiny-slider.js
 //= ../../node_modules/bootstrap-select/dist/js/bootstrap-select.min.js
 
 //= https://www.google.com/recaptcha/api.js
